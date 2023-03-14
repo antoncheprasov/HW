66fa9ba0ea82fd0c6a4fe561a9a88e10deebf6ff
antoncheprasov <126571304+antoncheprasov@users.noreply.github.com>
2023-03-14 15:13:48 +0100

    Add files via upload

---
+++ homeworktentoeightsystem.java
@@ -0,0 +1,55 @@
+import javax.swing.plaf.metal.MetalTabbedPaneUI;
+
+public class homeworktentoeightsystem {
+
+    public static void main(String[] args) {
+
+        int a = 1144;
+        //10 -> 8
+        System.out.println(1144 / 8);  //143
+        System.out.println(1144 % 8);  //0
+
+        System.out.println(143 / 8);  //17
+        System.out.println(143 % 8);  //7
+
+        System.out.println(17/ 8);  //2
+        System.out.println(17 % 8);  //1
+
+        System.out.println(2 / 8);  //0
+        System.out.println(2 % 8);  //2
+
+        int b = 978;
+        System.out.println(978 / 8);  //122
+        System.out.println(978 % 8);  //2
+
+        System.out.println(122 / 8);  //15
+        System.out.println(122 % 8);  //2
+
+        System.out.println(15 / 8);  //1
+        System.out.println(15 % 8);  //7
+
+        System.out.println(1 / 8);  //0
+        System.out.println(1 % 8);  //1
+
+        int c = 12;
+
+        System.out.println(12 / 8);  //1
+        System.out.println(12 % 8);  //4
+
+        System.out.println(1 / 8);  //0
+        System.out.println(1 % 8);  //1
+
+        int a1 = 2170;
+
+        System.out.println(2*Math.pow(8,3) + 1*Math.pow(8,2) + 7*Math.pow(8,1) + 0*Math.pow(8,0));  //1144
+
+        int b1 = 1722;
+
+        System.out.println(1*Math.pow(8,3) + 7*Math.pow(8,2) + 2*Math.pow(8,1) + 2*Math.pow(8,0));   //978
+
+        int c1 = 14;
+
+        System.out.println(1*Math.pow(8,1) + 4*Math.pow(8,0));  //12
+
+    }
+}
