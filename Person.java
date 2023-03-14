4e3716d9f6bb573c88635a0817300dc18b8107f0
antoncheprasov <126571304+antoncheprasov@users.noreply.github.com>
2023-03-14 14:36:24 +0100

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,15 @@
+public class Person {
+
+    public static void main(String[] args) {
+
+        Person person;
+
+        String name = "Anton"; // имя
+        String surname = "Cheprasov"; //фамилия
+        int yearOfBirth = 1986; //дата рождения
+
+        System.out.println(name);
+        System.out.println(surname);
+        System.out.println(yearOfBirth);
+    }
+}
