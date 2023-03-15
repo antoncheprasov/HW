b29c2ae819ea52f3bad4e125661934c51d9f0dad
antoncheprasov <126571304+antoncheprasov@users.noreply.github.com>
2023-03-15 20:33:20 +0100

    Add files via upload

---
+++ BasicCourseHW.java
@@ -0,0 +1,17 @@
+public class BasicCourseHW {
+
+    public static void main(String[] args) {
+
+        String Line = "Hello, Java! It's a Java Basic course";
+
+        System.out.println(Line.length());
+        System.out.println("!");
+        System.out.println("It's a Java");
+        System.out.println("Java Basic course");
+
+
+
+
+
+    }
+}
