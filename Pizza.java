f1137efe307dbef8bdae14f6de6e817ae6583439
antoncheprasov <126571304+antoncheprasov@users.noreply.github.com>
2023-03-20 22:34:25 +0100

    Add files via upload

---
+++ Pizza.java
@@ -0,0 +1,20 @@
+public class Pizza {
+
+    public static double main(String[] args) {
+
+        public Pizza(double input) {}
+
+
+        public double pow(double d) {return; Math.pow(Math.E, d); }
+
+        public double div(double e) {return d / 4; }
+
+        public Pizza = e * Math.PI;
+
+        System.out.println("Result is " + Pizza);
+
+
+
+    }
+
+}
